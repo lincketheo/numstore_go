da449cb462f79e04b3d0b5467541563cbfc93340
Theo Lincke <lincketheo@gmail.com>
2025-04-07 08:29:26 -0400

    adds

---
+++ internal/compiler/parseRange.go
@@ -0,0 +1,28 @@
+package compiler
+
+/*
+Range string:
+- start:stop:step
+- start:
+- :stop
+- start::step
+
+a[0]
+a[0, 1, 2] - each index(i) < dim[i]
+a[:, 1, 2]
+a[:]
+a[0:1:2]
+*/
+
+type dimRange struct {
+	start        int
+	stop         int
+	step         int
+	startPresent bool
+	stopPresent  bool
+	stepPresent  bool
+}
+
+type range struct {
+  dimRange []ranges
+}
--- internal/compiler/parseRead.go
+++ internal/compiler/parseRead.go
@@ -1,5 +1,5 @@
 package compiler
 
 func (p *parser) parseReadFormat() {
-
+  
 }
